2f1188315a9a50f4e4450e318d34dce1913cfc72
경아 <yookyungah@gyeong-aui-MacBookAir.local>
2024-04-27 14:35:41 +0900

    FEAT: 엘리스 / 메모 등록, 삭제, 검색 기능 구현

---
+++ script.js
@@ -0,0 +1,58 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.memos');
+const alertmsg = document.getElementById('alertmsg');
+const search = document.querySelector('.search input');
+
+const saveMemo = memoText => {
+    const html = `<li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span>${memoText}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>`
+
+    list.innerHTML += html;
+}
+
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const memo = addForm.add.value;
+    if (memo.length) {
+        saveMemo(memo);
+        alertmsg.classList.add('hidden');
+    }
+
+    else {
+        //글 내용을 작성하라는 메세지
+        // console.log('글 내용을 작성하세요')
+        alertmsg.classList.remove('hidden');
+    }
+
+    // console.log('글을 남겼습니다');
+    //글을 작성하기 위한
+});
+
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+const filterMemo = memo => {
+    // console.log(Array.from(list.children));
+
+    Array.from(list.children)
+        .filter((memotext) => !memotext.textContent.includes(memo))
+        .forEach((memotext) => memotext.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter((memotext) => memotext.textContent.includes(memo))
+        .forEach((memotext) => memotext.classList.remove('filtered'));
+}
+
+search.addEventListener('keyup', e => {
+    const searchText = search.value;
+    // console.log(searchText);
+    filterMemo(searchText);
+})
